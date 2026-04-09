2567e8844d3ceb6b5e4c7850cc31c7f927c6d37d
Brody Walker <blwalker43@tntech.edu>
2026-04-09 10:15:15 -0500

    Add placeholder for branch and bound algorithm

---
+++ Code/branch_and_bound.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// branch and bound algorithm implementation
+
+int main() {
+    cout << "branch and bound algorithm placeholder" << endl;
+    return 0;
+}
