36a5d98a750189967c7b72d4a35ea1afc84f9f36
Dante Haman <dhaman43@tntech.edu>
2026-04-27 08:45:52 -0500

    Updated random ranges in datasets.py

--- Code/datasets.py
+++ Code/datasets.py
@@ -2,7 +2,7 @@ import random
 
 def generate_dataset(n, capacity, seed=42):
     random.seed(seed)
-    return [random.randint(1, capacity) for _ in range(n)]
+    return [random.randint(1, 50) for _ in range(n)]
 
 def hard_dataset(n, capacity):
     return [capacity//2 + 1 for _ in range(n)]
